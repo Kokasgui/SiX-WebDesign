7f247653c861e4568735dbe6ced907da908662d4
Mariana Ferreira <maryanaferreyra@gmail.com>
2024-12-11 01:17:11 +0000

    Main Feature Desktop changes. Added moveble circle

--- assets/script/script.js
+++ assets/script/script.js
@@ -93,3 +93,13 @@ function changeSlide(direction) {
 //   // Mostra o slide atual
 //   slides[slideIndex - 1].style.display = "block";
 // }
+
+const cursor = document.querySelector("#cursor");
+const moveCursor = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+
+  cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
+
+window.addEventListener("mousemove", moveCursor);
